a89c624cc8352c3c61504fc0cbece82a5cb82fd8
tuanvu1992 <tuanvu.ksvt92@gmail.com>
2015-08-21 13:13:28 +0700

    Modified .gitignore to bypass bin/ folder

--- src/Sound.cc
+++ src/Sound.cc
@@ -93,72 +93,72 @@ int Sound::set_hwparams(snd_pcm_t *handle,
 	/* choose all parameters */
 	err = snd_pcm_hw_params_any(handle, params);
 	if (err < 0) {
-		printf("Broken configuration for playback: no configurations available: %s\n", snd_strerror(err));
+		DEBUG_ERROR("Broken configuration for playback: no configurations available: %s\n", snd_strerror(err));
 		return err;
 	}
 	/* set hardware resampling */
 	err = snd_pcm_hw_params_set_rate_resample(handle, params, Sound::resample);
 	if (err < 0) {
-		printf("Resampling setup failed for playback: %s\n", snd_strerror(err));
+		DEBUG_ERROR("Resampling setup failed for playback: %s\n", snd_strerror(err));
 		return err;
 	}
 	/* set the interleaved read/write format */
 	err = snd_pcm_hw_params_set_access(handle, params, access);
 	if (err < 0) {
-		printf("Access type not available for playback: %s\n", snd_strerror(err));
+		DEBUG_ERROR("Access type not available for playback: %s\n", snd_strerror(err));
 		return err;
 	}
 	/* set the sample format */
 	err = snd_pcm_hw_params_set_format(handle, params, format);
 	if (err < 0) {
-		printf("Sample format not available for playback: %s\n", snd_strerror(err));
+		DEBUG_ERROR("Sample format not available for playback: %s\n", snd_strerror(err));
 		return err;
 	}
 	/* set the count of channels */
 	err = snd_pcm_hw_params_set_channels(handle, params, channels);
 	if (err < 0) {
-		printf("Channels count (%i) not available for playbacks: %s\n", channels, snd_strerror(err));
+		DEBUG_ERROR("Channels count (%i) not available for playbacks: %s\n", channels, snd_strerror(err));
 		return err;
 	}
 	/* set the stream rate */
 	rrate = rate;
 	err = snd_pcm_hw_params_set_rate_near(handle, params, &rrate, 0);
 	if (err < 0) {
-		printf("Rate %iHz not available for playback: %s\n", rate, snd_strerror(err));
+		DEBUG_ERROR("Rate %iHz not available for playback: %s\n", rate, snd_strerror(err));
 		return err;
 	}
 	if (rrate != rate) {
-		printf("Rate doesn't match (requested %iHz, get %iHz)\n", rate, err);
+		DEBUG_ERROR("Rate doesn't match (requested %iHz, get %iHz)\n", rate, err);
 		return -EINVAL;
 	}
 	/* set the buffer time */
 	err = snd_pcm_hw_params_set_buffer_time_near(handle, params, &buffer_time, &dir);
 	if (err < 0) {
-		printf("Unable to set buffer time %i for playback: %s\n", buffer_time, snd_strerror(err));
+		DEBUG_ERROR("Unable to set buffer time %i for playback: %s\n", buffer_time, snd_strerror(err));
 		return err;
 	}
 	err = snd_pcm_hw_params_get_buffer_size(params, &size);
 	if (err < 0) {
-		printf("Unable to get buffer size for playback: %s\n", snd_strerror(err));
+		DEBUG_ERROR("Unable to get buffer size for playback: %s\n", snd_strerror(err));
 		return err;
 	}
 	buffer_size = size;
 	/* set the period time */
 	err = snd_pcm_hw_params_set_period_time_near(handle, params, &period_time, &dir);
 	if (err < 0) {
-		printf("Unable to set period time %i for playback: %s\n", period_time, snd_strerror(err));
+		DEBUG_ERROR("Unable to set period time %i for playback: %s\n", period_time, snd_strerror(err));
 		return err;
 	}
 	err = snd_pcm_hw_params_get_period_size(params, &size, &dir);
 	if (err < 0) {
-		printf("Unable to get period size for playback: %s\n", snd_strerror(err));
+		DEBUG_ERROR("Unable to get period size for playback: %s\n", snd_strerror(err));
 		return err;
 	}
 	period_size = size;
 	/* write the parameters to device */
 	err = snd_pcm_hw_params(handle, params);
 	if (err < 0) {
-		printf("Unable to set hw params for playback: %s\n", snd_strerror(err));
+		DEBUG_ERROR("Unable to set hw params for playback: %s\n", snd_strerror(err));
 		return err;
 	}
 	return 0;
@@ -170,35 +170,35 @@ int Sound::set_swparams(snd_pcm_t *handle, snd_pcm_sw_params_t *swparams)
 	/* get the current swparams */
 	err = snd_pcm_sw_params_current(handle, swparams);
 	if (err < 0) {
-		printf("Unable to determine current swparams for playback: %s\n", snd_strerror(err));
+		DEBUG_ERROR("Unable to determine current swparams for playback: %s\n", snd_strerror(err));
 		return err;
 	}
 	/* start the transfer when the buffer is almost full: */
 	/* (buffer_size / avail_min) * avail_min */
 	err = snd_pcm_sw_params_set_start_threshold(handle, swparams, (buffer_size / period_size) * period_size);
 	if (err < 0) {
-		printf("Unable to set start threshold mode for playback: %s\n", snd_strerror(err));
+		DEBUG_ERROR("Unable to set start threshold mode for playback: %s\n", snd_strerror(err));
 		return err;
 	}
 	/* allow the transfer when at least period_size samples can be processed */
 	/* or disable this mechanism when period event is enabled (aka interrupt like style processing) */
 	err = snd_pcm_sw_params_set_avail_min(handle, swparams, period_event ? buffer_size : period_size);
 	if (err < 0) {
-		printf("Unable to set avail min for playback: %s\n", snd_strerror(err));
+		DEBUG_ERROR("Unable to set avail min for playback: %s\n", snd_strerror(err));
 		return err;
 	}
 	/* enable period events when requested */
 	if (period_event) {
 		err = snd_pcm_sw_params_set_period_event(handle, swparams, 1);
 		if (err < 0) {
-			printf("Unable to set period event: %s\n", snd_strerror(err));
+			DEBUG_ERROR("Unable to set period event: %s\n", snd_strerror(err));
 			return err;
 		}
 	}
 	/* write the parameters to the playback device */
 	err = snd_pcm_sw_params(handle, swparams);
 	if (err < 0) {
-		printf("Unable to set sw params for playback: %s\n", snd_strerror(err));
+		DEBUG_ERROR("Unable to set sw params for playback: %s\n", snd_strerror(err));
 		return err;
 	}
 	return 0;
