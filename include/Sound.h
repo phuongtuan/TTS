da033b5b2fbd48bd572798198dbf292495d91574
tuanvu1992 <tuanvu.ksvt92@gmail.com>
2015-08-21 11:22:04 +0700

    Fix sound buffer issue when play wav file

--- include/Sound.h
+++ include/Sound.h
@@ -17,8 +17,25 @@ public:
 	Sound();
 	virtual ~Sound();
 	static int play(FILE *wav_file);
+	static int init();
+	static int set_hwparams(snd_pcm_t *handle, snd_pcm_hw_params_t *params, snd_pcm_access_t access);
+	static int set_swparams(snd_pcm_t *handle, snd_pcm_sw_params_t *swparams);
 private:
-	snd_output_t *output = NULL;
+	static char *device;					/* playback device */
+	static snd_pcm_format_t format;			/* sample format */
+	static unsigned int rate;				/* stream rate */
+	static unsigned int channels;			/* count of channels */
+	static unsigned int buffer_time;		/* ring buffer length in us */
+	static unsigned int period_time;		/* period time in us */
+	static int verbose;						/* verbose flag */
+	static int resample;					/* enable alsa-lib resampling */
+	static int period_event;				/* produce poll event after each period */
+
+	static snd_pcm_sframes_t buffer_size;
+	static snd_pcm_sframes_t period_size;
+	static snd_output_t *output;
+
+
 };
 
 } /* namespace iHearTech */
--- src/Sound.cc
+++ src/Sound.cc
@@ -10,6 +10,18 @@
 
 namespace iHearTech {
 
+char *Sound::device = (char*)"default";					/* playback device */
+snd_pcm_format_t Sound::format = SND_PCM_FORMAT_S16;	/* sample format */
+unsigned int Sound::rate = 16000;						/* stream rate */
+unsigned int Sound::channels = 1;						/* count of channels */
+unsigned int Sound::buffer_time = 500000;				/* ring buffer length in us */
+unsigned int Sound::period_time = 100000;				/* period time in us */
+int Sound::verbose = 0;									/* verbose flag */
+int Sound::resample = 1;								/* enable alsa-lib resampling */
+int Sound::period_event = 0;							/* produce poll event after each period */
+snd_pcm_sframes_t Sound::buffer_size;
+snd_pcm_sframes_t Sound::period_size;
+
 Sound::Sound() {
 	// TODO Auto-generated constructor stub
 }
@@ -20,47 +32,177 @@ Sound::~Sound() {
 
 int Sound::play(FILE *wav_file){
 	DEBUG_INFO("Playing wav file");
-	int err;
 	snd_pcm_t *handle;
-	snd_pcm_sframes_t frames;
-	if(wav_file == NULL){
-		DEBUG_ERROR("wav_file == NULL, return now");
-		return -1;
-	}
-	if ((err = snd_pcm_open(&handle, "default", SND_PCM_STREAM_PLAYBACK, 0)) < 0) {
+	int err;
+	snd_pcm_hw_params_t *hwparams;
+	snd_pcm_sw_params_t *swparams;
+	signed short *samples;
+	snd_pcm_hw_params_alloca(&hwparams);
+	snd_pcm_sw_params_alloca(&swparams);
+	if ((err = snd_pcm_open(&handle, device, SND_PCM_STREAM_PLAYBACK, 0)) < 0) {
 		DEBUG_ERROR("Playback open error: %s\n", snd_strerror(err));
+		return 0;
+	}
+	if ((err = set_hwparams(handle, hwparams, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) {
+		DEBUG_ERROR("Setting of hwparams failed: %s\n", snd_strerror(err));
+		return 0;
+	}
+	if ((err = set_swparams(handle, swparams)) < 0) {
+		DEBUG_ERROR("Setting of swparams failed: %s\n", snd_strerror(err));
+		return 0;
+	}
+	int samples_size = (period_size * channels * snd_pcm_format_physical_width(format)) / 8;
+	samples = (signed short *)malloc(samples_size);
+	if (samples == NULL) {
+		DEBUG_ERROR("No enough memory\n");
 		return -1;
 	}
-	if ((err = snd_pcm_set_params(handle,
-			SND_PCM_FORMAT_S16_LE,
-			SND_PCM_ACCESS_RW_INTERLEAVED,
-			1,
-			16000,
-			1,
-			500000)) < 0) {	/* 0.5sec */
-		DEBUG_ERROR("Playback open error: %s\n", snd_strerror(err));
-		return -1;
-	}
-	unsigned char *buffer = new unsigned char [BUFFER_SIZE];
-	if(buffer == NULL){
-		DEBUG_ERROR("Cannot allocate buffer memory");
-		return -1;
-	}
-	fseek(wav_file, 44, SEEK_SET);
+	int byte_read;
 	while(feof(wav_file) == 0){
-		memset(buffer, 0, BUFFER_SIZE);
-		fread((void*)buffer, sizeof(char), BUFFER_SIZE, wav_file);
-		frames = snd_pcm_writei(handle, buffer, BUFFER_SIZE);
-		if (frames < 0)
-			frames = snd_pcm_recover(handle, frames, 0);
-		if (frames < 0) {
-			DEBUG_ERROR("snd_pcm_writei failed: %s\n", snd_strerror(err));
-			break;
+		byte_read = fread(samples, sizeof(char), samples_size, wav_file);
+		signed short *ptr = samples;
+		int cptr;
+		if(byte_read < samples_size){
+			cptr = byte_read / ((channels * snd_pcm_format_physical_width(format))/8);
+		}else cptr = period_size;
+		while(cptr > 0){
+			err = snd_pcm_writei(handle, ptr, cptr);
+			if (err < 0) {
+				err = snd_pcm_recover(handle, err, 0);
+			}
+			if(err < 0){
+				DEBUG_ERROR("snd_pcm_writei failed: %s\n", snd_strerror(err));
+				break;
+			}
+			ptr += err * channels;
+			cptr -= err;
 		}
 	}
 	snd_pcm_close(handle);
-	delete[] buffer;
+	free(samples);
 	return 0;
 }
+int Sound::set_hwparams(snd_pcm_t *handle,
+		snd_pcm_hw_params_t *params,
+		snd_pcm_access_t access)
+{
+	unsigned int rrate;
+	snd_pcm_uframes_t size;
+	int err, dir;
+
+	/* choose all parameters */
+	err = snd_pcm_hw_params_any(handle, params);
+	if (err < 0) {
+		printf("Broken configuration for playback: no configurations available: %s\n", snd_strerror(err));
+		return err;
+	}
+	/* set hardware resampling */
+	err = snd_pcm_hw_params_set_rate_resample(handle, params, Sound::resample);
+	if (err < 0) {
+		printf("Resampling setup failed for playback: %s\n", snd_strerror(err));
+		return err;
+	}
+	/* set the interleaved read/write format */
+	err = snd_pcm_hw_params_set_access(handle, params, access);
+	if (err < 0) {
+		printf("Access type not available for playback: %s\n", snd_strerror(err));
+		return err;
+	}
+	/* set the sample format */
+	err = snd_pcm_hw_params_set_format(handle, params, format);
+	if (err < 0) {
+		printf("Sample format not available for playback: %s\n", snd_strerror(err));
+		return err;
+	}
+	/* set the count of channels */
+	err = snd_pcm_hw_params_set_channels(handle, params, channels);
+	if (err < 0) {
+		printf("Channels count (%i) not available for playbacks: %s\n", channels, snd_strerror(err));
+		return err;
+	}
+	/* set the stream rate */
+	rrate = rate;
+	err = snd_pcm_hw_params_set_rate_near(handle, params, &rrate, 0);
+	if (err < 0) {
+		printf("Rate %iHz not available for playback: %s\n", rate, snd_strerror(err));
+		return err;
+	}
+	if (rrate != rate) {
+		printf("Rate doesn't match (requested %iHz, get %iHz)\n", rate, err);
+		return -EINVAL;
+	}
+	/* set the buffer time */
+	err = snd_pcm_hw_params_set_buffer_time_near(handle, params, &buffer_time, &dir);
+	if (err < 0) {
+		printf("Unable to set buffer time %i for playback: %s\n", buffer_time, snd_strerror(err));
+		return err;
+	}
+	err = snd_pcm_hw_params_get_buffer_size(params, &size);
+	if (err < 0) {
+		printf("Unable to get buffer size for playback: %s\n", snd_strerror(err));
+		return err;
+	}
+	buffer_size = size;
+	/* set the period time */
+	err = snd_pcm_hw_params_set_period_time_near(handle, params, &period_time, &dir);
+	if (err < 0) {
+		printf("Unable to set period time %i for playback: %s\n", period_time, snd_strerror(err));
+		return err;
+	}
+	err = snd_pcm_hw_params_get_period_size(params, &size, &dir);
+	if (err < 0) {
+		printf("Unable to get period size for playback: %s\n", snd_strerror(err));
+		return err;
+	}
+	period_size = size;
+	/* write the parameters to device */
+	err = snd_pcm_hw_params(handle, params);
+	if (err < 0) {
+		printf("Unable to set hw params for playback: %s\n", snd_strerror(err));
+		return err;
+	}
+	return 0;
+}
+
+int Sound::set_swparams(snd_pcm_t *handle, snd_pcm_sw_params_t *swparams)
+{
+	int err;
+	/* get the current swparams */
+	err = snd_pcm_sw_params_current(handle, swparams);
+	if (err < 0) {
+		printf("Unable to determine current swparams for playback: %s\n", snd_strerror(err));
+		return err;
+	}
+	/* start the transfer when the buffer is almost full: */
+	/* (buffer_size / avail_min) * avail_min */
+	err = snd_pcm_sw_params_set_start_threshold(handle, swparams, (buffer_size / period_size) * period_size);
+	if (err < 0) {
+		printf("Unable to set start threshold mode for playback: %s\n", snd_strerror(err));
+		return err;
+	}
+	/* allow the transfer when at least period_size samples can be processed */
+	/* or disable this mechanism when period event is enabled (aka interrupt like style processing) */
+	err = snd_pcm_sw_params_set_avail_min(handle, swparams, period_event ? buffer_size : period_size);
+	if (err < 0) {
+		printf("Unable to set avail min for playback: %s\n", snd_strerror(err));
+		return err;
+	}
+	/* enable period events when requested */
+	if (period_event) {
+		err = snd_pcm_sw_params_set_period_event(handle, swparams, 1);
+		if (err < 0) {
+			printf("Unable to set period event: %s\n", snd_strerror(err));
+			return err;
+		}
+	}
+	/* write the parameters to the playback device */
+	err = snd_pcm_sw_params(handle, swparams);
+	if (err < 0) {
+		printf("Unable to set sw params for playback: %s\n", snd_strerror(err));
+		return err;
+	}
+	return 0;
+}
+
 
 } /* namespace iHearTech */
